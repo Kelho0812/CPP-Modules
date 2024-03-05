9cc6bf65ada817e5c73c056cb44d4958bb633802
Kelho <Kelho88@pm.me>
2024-03-05 16:02:25 +0000

    Update General.h includes

--- C00/ex01/includes/General.h
+++ C00/ex01/includes/General.h
@@ -10,10 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
 using std::string;
 using std::toupper;
