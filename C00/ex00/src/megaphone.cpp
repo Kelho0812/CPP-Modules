25dc38f410c5e5707bc40fb0ca165f21cef2ead3
Kelho <Kelho88@pm.me>
2024-03-05 10:22:05 +0000

    ex00 done

--- C00/ex00/src/megaphone.cpp
+++ C00/ex00/src/megaphone.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
+using std::cout;
+using std::endl;
+using std::string;
+using std::toupper;
+
+char	*to_upper_string(char *str);
+
 int	main(int argc, char const *argv[])
 {
-	(void)argc;
-	(void)argv;
-	std::cout << "HELLO WORLD" << std::endl;
+	string	capslock_str;
+	string	loud_noise;
+
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			capslock_str = argv[i];
+			for (size_t j = 0; j < capslock_str.size(); ++j)
+				capslock_str[j] = toupper(capslock_str[j]);
+			cout << capslock_str;
+		}
+		cout << endl;
+	}
+	else
+	{
+		loud_noise = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		cout << loud_noise << endl;
+	}
 	return (0);
 }
