6b5fec7060f04a1056ff6950c41bb72eb222c940
Kelho <Kelho88@pm.me>
2024-03-05 16:02:37 +0000

    Add PhoneBook class and its header file

--- C00/ex01/includes/PhoneBook.h
+++ C00/ex01/includes/PhoneBook.h
@@ -1,36 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.h                                        :+:      :+:    :+:   */
+/*   PhoneBook.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jorteixe  <jorteixe@student.42porto.>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 11:06:46 by jorteixe          #+#    #+#             */
-/*   Updated: 2024/03/05 11:07:10 by jorteixe         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:39:14 by jorteixe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
 
+# include "Contact.h"
 # include "General.h"
 
-class Phonebook
+void	print_titles(void);
+void	print_all(Contact *c_array);
+void	print_single(Contact *c_array, int i);
+
+class PhoneBook
 {
-private:
-	/* data */
-public:
-	Phonebook(/* args */);
-	~Phonebook();
+  private:
+	Contact c_array[8];
+	int index;
+
+  public:
+	PhoneBook();
+	~PhoneBook();
+	void print();
 };
-
-Phonebook::Phonebook(/* args */)
-{
-}
-
-Phonebook::~Phonebook()
-{
-}
-
 
 #endif
